1d7d85dc5768c1161880c838cb61f2b5012da4be
Luke <mythnan@gmail.com>
2018-08-31 10:04:56 +1000

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 	var now = new Date().toString();
@@ -30,9 +29,11 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use((req, res, next) => {
-	res.render('maintaince.hbs');
-});
+// app.use((req, res, next) => {
+// 	res.render('maintaince.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
